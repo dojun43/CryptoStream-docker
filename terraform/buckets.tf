765fd10a5637bd98e6f8a7a1014dab9c4b90b127
dojun43 <dojun43@gmail.com>
2025-02-16 20:37:45 +0900

    add parquet bucket

--- terraform/buckets.tf
+++ terraform/buckets.tf
@@ -1,18 +1,38 @@
-resource "google_storage_bucket" "upbit-orderbook" {
-  name                        = "upbit-orderbook-1"
+resource "google_storage_bucket" "upbit-orderbook-cts" {
+  name                        = "upbit-orderbook-cts-1"
   location                    = var.region
   storage_class               = "STANDARD"
   uniform_bucket_level_access = true
   public_access_prevention = "enforced"
 
-  # 30일 지난 객체 스토리지 클래스 변경
+  # 7일 지난 객체 스토리지 클래스 변경
   lifecycle_rule {
     action {
       type = "SetStorageClass"
       storage_class = "NEARLINE"    
     }
     condition {
-      age = 30 
+      age = 7 
+    }
+  }
+}
+
+
+resource "google_storage_bucket" "upbit-orderbook-cts-parquet" {
+  name                        = "upbit-orderbook-cts-parquet-1"
+  location                    = var.region
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+  public_access_prevention = "enforced"
+
+  # 7일 지난 객체 스토리지 클래스 변경
+  lifecycle_rule {
+    action {
+      type = "SetStorageClass"
+      storage_class = "NEARLINE"    
+    }
+    condition {
+      age = 7 
     }
   }
 }
